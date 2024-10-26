ad8d1b212a37885ea6957b2e76bf4445a89956b0
SouvagyaDash29 <souvagyadash1999@gmail.com>
2024-10-26 20:26:03 +0530

    create function for new user chats and chat's title

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const { imageUpload  } = require('../controllers/chatController');
+const { imageUpload, chat, userChats, chats } = require('../controllers/chatController');
+const authenticateToken=require('../middlewares/authenticateToken')
 
 const router = express.Router();
 
 router.get('/upload', imageUpload);
+router.post('/chats',authenticateToken , chat);
+router.get('/userchats',authenticateToken, userChats);
+
 
 module.exports = router;
